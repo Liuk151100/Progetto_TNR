db229590286e77e78ae6e4aea25103fd133cab40
Luca Faini <lucafaini20@gmail.com>
2025-10-26 11:30:05 +0100

    modifica testo pulsante login

--- Progetto_TNR_Frontend/src/components/Header.jsx
+++ Progetto_TNR_Frontend/src/components/Header.jsx
@@ -26,7 +26,7 @@ const UserMenu = ({ token, logout, loggedUser }) => {
         className="ms-3 fw-bold"
         style={{ borderRadius: "20px" }}
       >
-        Login
+        Login//Register
       </Button>
     );
   }
