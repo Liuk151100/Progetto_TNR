706658296c8e4e3545cc019b40f2c6ed572afd7e
Luca Faini <lucafaini20@gmail.com>
2025-10-26 09:39:56 +0100

    correzione errore helper sendMail

--- Progetto_TNR_Backend/helpers/sendMail.js
+++ Progetto_TNR_Backend/helpers/sendMail.js
@@ -3,7 +3,7 @@ import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export async function sendEmail({ to, subject, text, html }) {
+export async function sendEmail({ to, subject, text, html, from }) {
   const msg = {
     to,
     from,
